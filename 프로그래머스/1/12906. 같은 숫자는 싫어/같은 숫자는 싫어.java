4289ea17d2eff8748a7a3584beec9d8f28e0dafa
veronees <151748893+veronees@users.noreply.github.com>
2024-05-09 17:46:52 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 31.99 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        List<Integer> list = new ArrayList<>();
+        
+        list.add(arr[0]);
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (list.get(list.size() - 1) != arr[i]) {
+                list.add(arr[i]);
+            }
+        }
+        
+        int[] answer = new int[list.size()];
+        
+        for (int j = 0; j < list.size(); j++) {
+            answer[j] = list.get(j);
+        }
+
+        return answer;
+    }
+}
