ba27448dd1ef5cd1dc1284d753ba27798febb572
veronees <151748893+veronees@users.noreply.github.com>
2024-09-05 18:59:05 +0900

    [Silver III] Title: 1로 만들기, Time: 116 ms, Memory: 18324 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int[] dp = new int[1000001];
+   
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(reader.readLine());
+
+        for (int i = 2; i <= num; i++) {
+            dp[i] = dp[i - 1] + 1;
+            
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+        }
+        System.out.println(dp[num]);
+    }
+}
