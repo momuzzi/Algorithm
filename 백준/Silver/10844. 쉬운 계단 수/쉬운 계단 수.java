f433a1a3cd5b69b2b8a09d6b48165b9bd0b7dcd5
veronees <151748893+veronees@users.noreply.github.com>
2024-09-14 17:47:46 +0900

    Update 쉬운 계단 수.java
    [Silver I] Title: 쉬운 계단 수, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
@@ -12,11 +11,11 @@ public class Main {
 
         int[][] dp = new int[N + 1][10]; // [1 ~ N][0 ~ 9] [N일 때][젤 뒷자리가 x인 경우]
 
-        dp[1][0] = 0 % 1000000000; //N이 1일 때 가장 뒷자리가 0인경우는 0개
+        dp[1][0] = 0; //N이 1일 때 가장 뒷자리가 0인경우는 0개
 
         // dp[1][1~9] = 1로 초기화
         for (int i = 1; i <= 9; i++) {
-            dp[1][i] = 1 % 1000000000;
+            dp[1][i] = 1;
         }
 
         if (N == 1) {
@@ -27,11 +26,11 @@ public class Main {
         for (int i = 2; i <= N; i++) {
             for (int j = 0; j <= 9; j++) {
                 if (j == 0) {
-                    dp[i][j] = dp[i - 1][j + 1] % 1000000000;
+                    dp[i][j] = dp[i - 1][j + 1];
                     continue;
                 }
                 if (j == 9) {
-                    dp[i][j] = dp[i - 1][j - 1] % 1000000000;
+                    dp[i][j] = dp[i - 1][j - 1];
                     continue;
                 }
 
