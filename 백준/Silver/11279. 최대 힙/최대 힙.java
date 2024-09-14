5ffd97127d047f041827ad8b0ef6657cd04b8962
veronees <151748893+veronees@users.noreply.github.com>
2024-09-14 18:52:57 +0900

    [Silver II] Title: 최대 힙, Time: 288 ms, Memory: 26200 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(reader.readLine());
+
+        StringBuilder builder = new StringBuilder();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 1; i <= N; i++) {
+            int x = Integer.parseInt(reader.readLine());
+
+            if (x != 0) {
+                pq.add(x);
+            } else {
+                if (pq.isEmpty()) {
+                    builder.append(0).append("\n");
+                } else {
+                    builder.append(pq.poll()).append("\n");
+                }
+            }
+        }
+
+        System.out.print(builder);
+    }
+}
