d3df089c99ae92eb30ed3c8fe047faf455cd5e72
veronees <151748893+veronees@users.noreply.github.com>
2024-09-17 22:55:22 +0900

    [Silver II] Title: N과 M (9), Time: 2376 ms, Memory: 24456 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] arr;
+    static int[] inputArr;
+    static boolean[] isUsed;
+    static int N;
+    static int M;
+    static StringBuilder builder;
+    static List<int[]> list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        builder = new StringBuilder();
+        String str = reader.readLine();
+        String[] s = str.split(" ");
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
+        isUsed = new boolean[N];
+        inputArr = new int[N];
+        arr = new int[M];
+        list = new ArrayList<>();
+
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine(), " ");
+
+        for (int i = 0; i < N; i++) {
+            inputArr[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+        Arrays.sort(inputArr);
+
+        bt(0);
+
+        for (int[] a : list) {
+            for (int i : a) {
+                builder.append(i).append(" ");
+            }
+            builder.append("\n");
+        }
+
+        System.out.print(builder);
+    }
+
+    static void bt(int index) {
+        if (index == M) {
+            int[] partArr = new int[M];
+
+            for (int j = 0; j < M; j++) {
+                partArr[j] = arr[j];
+            }
+
+            boolean isDup = false;
+            for (int[] a : list) {
+                if (Arrays.equals(a, partArr)) {
+                    isDup = true;
+                }
+            }
+
+            if (!isDup) {
+                list.add(partArr);
+            }
+
+            return;
+        }
+
+        for (int i = 0; i < N ; i++) {
+            if (!isUsed[i]) {
+                isUsed[i] = true;
+                arr[index] = inputArr[i];
+                bt(index + 1);
+                isUsed[i] = false;
+            }
+        }
+    }
+}
