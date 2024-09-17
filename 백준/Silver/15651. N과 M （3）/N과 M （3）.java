65bd127e33556d58f718fc4636fc922602038edd
veronees <151748893+veronees@users.noreply.github.com>
2024-09-17 20:07:53 +0900

    [Silver III] Title: N과 M (3), Time: 404 ms, Memory: 68496 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int[] arr;
+    static int N;
+    static int M;
+    static StringBuilder builder;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        builder = new StringBuilder();
+        String str = reader.readLine();
+        String[] s = str.split(" ");
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
+
+        arr = new int[M];
+        bt(0);
+
+        System.out.print(builder);
+    }
+
+    static void bt(int index) {
+        if (index == M) {
+            for (int num : arr) {
+                builder.append(num).append(" ");
+            }
+            builder.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            arr[index] = i;
+            bt(index + 1);
+        }
+    }
+}
