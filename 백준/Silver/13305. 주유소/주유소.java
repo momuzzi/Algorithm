02f0a79daff1db93394ff95e6e15f105a0894e9d
veronees <151748893+veronees@users.noreply.github.com>
2024-09-20 03:49:49 +0900

    [Silver III] Title: 주유소, Time: 356 ms, Memory: 35956 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -9,7 +9,7 @@ public class Main {
 
         int N = Integer.parseInt(reader.readLine());
 
-        int[] road = new int[N - 1];
+        long[] road = new long[N - 1];
         tokenizer = new StringTokenizer(reader.readLine(), " ");
         for (int i = 0; i < N - 1; i++) {
             road[i] = Integer.parseInt(tokenizer.nextToken());
@@ -21,15 +21,14 @@ public class Main {
             city[i] = Integer.parseInt(tokenizer.nextToken());
         }
 
-        int money = city[0] * road[0];
-        int minCity = city[0];
-        for (int i = 1; i < N - 1; i++) {
+        long money = 0;
+        long minCity = city[0];
+        for (int i = 0; i < N - 1; i++) {
             if (minCity > city[i]) {
-                money += city[i] * road[i];
                 minCity = city[i];
-            } else {
-                money += minCity * road[i];
             }
+
+            money += minCity * road[i];
         }
 
         System.out.print(money);
