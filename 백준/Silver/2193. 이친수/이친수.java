487c7573208d91b471e57cff03a1664d0116a453
veronees <151748893+veronees@users.noreply.github.com>
2024-09-22 03:20:40 +0900

    [Silver III] Title: 이친수, Time: 100 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(reader.readLine());
+
+        long[][] dp = new long[n + 1][2];
+
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+            dp[i][1] = dp[i - 1][0];
+        }
+
+        System.out.print(dp[n][0] + dp[n][1]);
+    }
+}
