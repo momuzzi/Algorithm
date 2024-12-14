864192832049b295827348da96cd98bd7a252788
vero <151748893+veronees@users.noreply.github.com>
2024-12-14 23:12:31 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14484 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,65 +1,61 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int sero;
-    static int garo;
-    static int[][] miroLocations;
-    static boolean[][] isVisited;
+
+    static int[][] graph;
+    static int[][] dist;
+    static int N;
+    static int M;
+    static Deque<int[]> q;
     static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1 ,0, 0};
-    static LinkedList<int[]> q;
-    static int[][] distance;
+    static int[] dy = {-1, 1, 0, 0};
 
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer = new StringTokenizer(reader.readLine(), " ");
-        sero = Integer.parseInt(tokenizer.nextToken()); // 4
-        garo = Integer.parseInt(tokenizer.nextToken()); // 6
-        miroLocations = new int[sero][garo];
-        isVisited = new boolean[sero][garo];
-        distance = new int[sero][garo];
-        for (int j = 0; j < sero; j++) {
-            String str = reader.readLine();
-            for (int k = 0; k < str.length(); k++) {
-                int validBlock = Character.getNumericValue(str.charAt(k)); // 0 or 1
-                miroLocations[j][k] = validBlock;
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = rd.readLine().split(" ");
+
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+
+        graph = new int[N + 1][M + 1];
+        dist = new int[N + 1][M + 1];
+        dist[1][1] = 1;
+
+        q = new ArrayDeque<>();
+
+        for (int i = 1; i <= N; i++) {
+            String str = rd.readLine();
+            for (int j = 1; j <= M; j++) {
+                graph[i][j] = Character.getNumericValue(str.charAt(j - 1));
             }
         }
 
-        bfs(0, 0);
+        q.addLast(new int[] {1, 1});
+        bfs();
 
-        System.out.print(distance[sero - 1][garo - 1]);
+        System.out.print(dist[N][M]);
     }
 
-    static void bfs(int l, int m) {
-        q = new LinkedList<>();
-        isVisited[l][m] = true;
-        q.add(new int[] {l, m});
-        distance[l][m] = 1;
+    static void bfs() {
         while (!q.isEmpty()) {
-            int[] poll = q.poll();
-            int x = poll[0];
-            int y = poll[1];
+            int[] nm = q.pollFirst();
+            int x = nm[0];
+            int y = nm[1];
 
             for (int i = 0; i < 4; i++) {
-                int moveDx = x + dx[i];
-                int moveDy = y + dy[i];
+                int moveX = x + dx[i];
+                int moveY = y + dy[i];
 
-                if (moveDx < 0 || moveDy < 0 || moveDx >= sero || moveDy >= garo) {
+                if (moveX < 1 || moveX > N || moveY < 1 || moveY > M) {
                     continue;
                 }
 
-                if (miroLocations[moveDx][moveDy] == 0 || isVisited[moveDx][moveDy]) {
-                    continue;
+                if (graph[moveX][moveY] == 1 && dist[moveX][moveY] == 0) {
+                    dist[moveX][moveY] = dist[x][y] + 1;
+                    q.addLast(new int[] {moveX, moveY});
                 }
-
-                isVisited[moveDx][moveDy] = true;
-                q.add(new int[] {moveDx, moveDy});
-                distance[moveDx][moveDy] = distance[x][y] + 1;
             }
         }
     }
