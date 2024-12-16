89b9bbd328a8d90ef42d90460d4d9fd4aaf69a02
vero <151748893+veronees@users.noreply.github.com>
2024-12-16 18:09:29 +0900

    [Gold IV] Title: N-Queen, Time: 2592 ms, Memory: 14776 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,52 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+
     static int N;
     static int cnt;
-    static int[] arr;
+    static boolean[] arr;
+    static boolean[] upArr;
+    static boolean[] downArr;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(reader.readLine());
-        arr = new int[N];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new boolean[N];
+        upArr = new boolean[2 * N - 1];
+        downArr = new boolean[2 * N - 1];
 
-        for (int i = 0; i < N; i++) {
-            arr[0] = i; //(0, i)에 퀸을 놓았다는 의미
-            bt(1); //0번째 줄은 이미 놓음, 1번째 줄부터 퀸을 하나씩 놓기 시도
-        }
+        bt(0);
 
         System.out.print(cnt);
     }
 
-    static void bt(int garo) {
-        if (garo == N) {
-            cnt++; //퀸을 전부 놓았으므로 경우의 수 1 증가
+    static void bt(int idx) {
+        if (idx == N) {
+            cnt++;
             return;
         }
 
-        for (int sero = 0; sero < N; sero++) {
-            if (safe(garo, sero)) {
-                arr[garo] = sero;
-                bt(garo + 1);
+        for (int i = 0; i < N; i++) {
+            if (!arr[i] && !upArr[idx + i] && !downArr[N - 1 + idx - i]) {
+                arr[i] = true;
+                upArr[idx + i] = true;
+                downArr[N - 1 + idx - i] = true;
+                bt(idx + 1);
+                arr[i] = false;
+                upArr[idx + i] = false;
+                downArr[N - 1 + idx - i] = false;
             }
         }
-    }
-
-    // (garo, sero) = (x, y)
-    static boolean safe(int x, int y) {
-        for (int i = 0; i < x; i++) {
-            // 같은 열에 위치한 퀸이 있는지 체크
-            if (arr[i] == y) {
-                return false;
-            }
-            // 대각선 체크
-            if (Math.abs(x - i) == Math.abs(y - arr[i])) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
