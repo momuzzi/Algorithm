ce6fdd1f4ac78092960322d1f8b189e06cad73d5
momuzzi <151748893+momuzzi@users.noreply.github.com>
2025-03-05 20:27:49 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 596 ms, Memory: 133768 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -0,0 +1,101 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int M;
+    static boolean[][] road;
+    static boolean[][][] visit;
+
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws Exception {
+        init();
+        solve();
+    }
+
+    static void init() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
+
+        road = new boolean[N][M];
+        visit = new boolean[2][N][M];
+
+        for (int i = 0; i < N; i++) {
+            String ss = br.readLine();
+            for (int j = 0; j < M; j++) {
+                road[i][j] = ss.charAt(j) == '0' ? true : false;
+            }
+        }
+    }
+
+    static void solve() {
+        System.out.print(bfs());
+    }
+
+    static int bfs() {
+        LinkedList<Node> q = new LinkedList<>();
+        q.offerLast(new Node(0, 0, 1, false));
+        visit[0][0][0] = true;
+        while (!q.isEmpty()) {
+            Node node = q.pollFirst();
+
+            if (node.x == N - 1 && node.y == M - 1) {
+                return node.dist;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int moveDx = node.x + dx[i];
+                int moveDy = node.y + dy[i];
+
+                if (moveDx < 0 || moveDy < 0 || moveDx >= N || moveDy >= M) continue;
+
+                // 길인 경우
+                if (road[moveDx][moveDy]) {
+                    // 벽을 부선적이 있고, 벽을 부순 방문 배열에서 방문 안한 경우
+                    if (node.brk && !visit[1][moveDx][moveDy]) {
+                        visit[1][moveDx][moveDy] = true;
+                        q.offerLast(new Node(moveDx, moveDy, node.dist + 1, node.brk));
+                    }
+
+                    // 벽을 부신적이 없고, 벽을 부수지 않은 방문 배열에서 방문 안한 경우
+                    if (!node.brk && !visit[0][moveDx][moveDy]) {
+                        visit[0][moveDx][moveDy] = true;
+                        q.offerLast(new Node(moveDx, moveDy, node.dist + 1, node.brk));
+                    }
+
+                    continue;
+                }
+
+                // 벽인 경우
+                if (!road[moveDx][moveDy]) {
+                    // 벽을 부신적이 없는 경우
+                    if (!node.brk) {
+                        visit[1][moveDx][moveDy] = true;
+                        q.offerLast(new Node(moveDx, moveDy, node.dist + 1, true));
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    static class Node {
+        int x;
+        int y;
+        int dist;
+        boolean brk;
+
+        public Node(int x, int y, int dist, boolean brk) {
+            this.x = x;
+            this.y = y;
+            this.dist = dist;
+            this.brk = brk;
+        }
+    }
+}
