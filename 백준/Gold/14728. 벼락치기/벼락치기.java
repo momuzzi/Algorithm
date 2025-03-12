9c8de338635054b2d84f0553d89facd1a8852aa1
momuzzi <151748893+momuzzi@users.noreply.github.com>
2025-03-12 23:03:49 +0900

    [Gold V] Title: 벼락치기, Time: 120 ms, Memory: 18196 KB -BaekjoonHub

---
+++ 백준/Gold/14728. 벼락치기/벼락치기.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int T;
+    static int[][] arr;
+    static int[][] dp;
+
+    public static void main(String[] args) throws Exception {
+        init();
+        solve();
+    }
+
+    static void init() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        N = Integer.parseInt(s[0]);
+        T = Integer.parseInt(s[1]);
+
+        arr = new int[N + 1][2];
+
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        dp = new int[N + 1][T + 1];
+    }
+
+    static void solve() {
+        int answer = 0;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= T; j++) {
+                dp[i][j] = dp[i - 1][j];
+
+                if (j >= arr[i][0]) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - arr[i][0]] + arr[i][1]);
+                }
+
+                answer = Math.max(answer, dp[i][j]);
+            }
+        }
+
+        System.out.print(answer);
+    }
+}
