bdeebbcccabc17219687ab4af0d1002bf3dbf974
momuzzi <151748893+momuzzi@users.noreply.github.com>
2025-07-31 20:04:34 +0900

    [level 2] Title: 배달, Time: 5.10 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,20 +2,18 @@ import java.util.*;
 
 class Solution {
     
-    static int[][] roads;
+    static List<List<Town>> roads = new ArrayList<>();
     static int[] totalTimeArr;
     static int MAX = 10000 * 50 + 1;
     
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
         
-        roads = new int[N + 1][N + 1];
         totalTimeArr = new int[N + 1];
         
-        for (int i = 1; i <= N; i++) {
-            Arrays.fill(roads[i], MAX);
+        for (int i = 0; i <= N; i++) {
             totalTimeArr[i] = MAX;
-            roads[i][i] = 0;
+            roads.add(new ArrayList<>());
         }
         
         for (int i = 0; i < road.length; i++) {
@@ -24,11 +22,10 @@ class Solution {
             int to = info[1];
             int need = info[2];
             
-            roads[from][to] = Math.min(roads[from][to], need);
-            roads[to][from] = Math.min(roads[to][from], need);
+            roads.get(from).add(new Town(to, need));
+            roads.get(to).add(new Town(from, need));
         }
         
-        totalTimeArr[1] = 0;
         dijk(1, N);
 
         for (int i = 1; i < totalTimeArr.length; i++) {
@@ -40,17 +37,16 @@ class Solution {
     
     static void dijk(int start, int N) {
         PriorityQueue<Town> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.time, b.time));
-        pq.offer(new Town(1, 0));
+        pq.offer(new Town(start, 0));
+        totalTimeArr[start] = 0;
         
         while (!pq.isEmpty()) {
             Town town = pq.poll();
             
-            for (int i = 1; i <= N; i++) {
-                if (town.num == i) continue;
-                                
-                if (town.time + roads[town.num][i] < totalTimeArr[i]) {
-                    totalTimeArr[i] = town.time + roads[town.num][i];
-                    pq.offer(new Town(i, totalTimeArr[i]));
+            for (Town nextTown : roads.get(town.num)) {
+                if (town.time + nextTown.time < totalTimeArr[nextTown.num]) {
+                    totalTimeArr[nextTown.num] = town.time + nextTown.time;
+                    pq.offer(new Town(nextTown.num, totalTimeArr[nextTown.num]));
                 }
             }
         }
