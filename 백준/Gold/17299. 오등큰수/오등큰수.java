3e8811b1361b4c6c807ffd41b0dc4972e98e000c
momuzzi <151748893+momuzzi@users.noreply.github.com>
2026-01-02 20:44:21 +0900

    [Gold III] Title: 오등큰수, Time: 1556 ms, Memory: 300580 KB -BaekjoonHub

---
+++ 백준/Gold/17299. 오등큰수/오등큰수.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        Map<Integer, Integer> map = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        int[] ans = new int[N];
+        for (int i = 0; i < N; i++) {
+
+            while (!stack.isEmpty() && map.get(arr[stack.peek()]) < map.get(arr[i])) {
+                int idx = stack.pop();
+
+                ans[idx] = arr[i];
+            }
+
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            ans[idx] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : ans) {
+            sb.append(i + " ");
+        }
+
+        System.out.print(sb);
+    }
+}
