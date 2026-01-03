6ccbaacaa327c605ed93813b7e7c9973775b9c16
momuzzi <151748893+momuzzi@users.noreply.github.com>
2026-01-03 21:32:30 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,9 @@
+SELECT ITEM_ID, ITEM_NAME, RARITY
+FROM ITEM_INFO
+WHERE ITEM_ID IN (SELECT it.ITEM_ID
+                  FROM ITEM_INFO ii 
+                  JOIN ITEM_TREE it 
+                  ON ii.ITEM_ID = it.PARENT_ITEM_ID
+                  WHERE ii.RARITY = 'RARE')
+ORDER BY ITEM_ID DESC                  
+                  
