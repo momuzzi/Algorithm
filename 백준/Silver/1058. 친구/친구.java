fb81883066e7e97c6bb92d9f5f57782d3e93811a
momuzzi <151748893+momuzzi@users.noreply.github.com>
2026-01-05 21:21:06 +0900

    [Silver II] Title: 친구, Time: 108 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Silver/1058. 친구/친구.java
@@ -0,0 +1,42 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] graph = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = s.charAt(j) == 'Y' ? 1 : Integer.MAX_VALUE;
+            }
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (i == j) continue;
+
+                    if (graph[i][k] != Integer.MAX_VALUE && graph[k][j] != Integer.MAX_VALUE) {
+                        graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+                    }
+                }
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            int cnt = 0;
+            for (int j = 0; j < N; j++) {
+                if (graph[i][j] <= 2) cnt++;
+            }
+
+            max = Math.max(max, cnt);
+        }
+
+        System.out.print(max);
+    }
+}
