38e7b58a34ab9dbc664a15f40655dc0c53512eb6
momuzzi <151748893+momuzzi@users.noreply.github.com>
2026-01-09 23:44:33 +0900

    [Gold IV] Title: 줄세우기, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    max = Math.max(max, dp[i]);
+                }
+            }
+        }
+
+        System.out.print(N - max);
+    }
+}
