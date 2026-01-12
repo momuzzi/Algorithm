c704b1223a3f8554211fcabb90556c85324d44c6
momuzzi <151748893+momuzzi@users.noreply.github.com>
2026-01-12 20:01:39 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 196 ms, Memory: 33780 KB -BaekjoonHub

---
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
@@ -0,0 +1,381 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class Result {
+        int rx;
+        int ry;
+        int bx;
+        int by;
+
+        Result(int rx, int ry, int bx, int by) {
+            this.rx = rx;
+            this.ry = ry;
+            this.bx = bx;
+            this.by = by;
+        }
+    }
+
+    static int N, M;
+    static char[][] board;
+    static int ans = -1;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] NM = br.readLine().split(" ");
+        N = Integer.parseInt(NM[0]);
+        M = Integer.parseInt(NM[1]);
+
+        board = new char[N][M];
+
+        int rx = 0;
+        int ry = 0;
+        int bx = 0;
+        int by = 0;
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < M; j++) {
+                board[i][j] = s.charAt(j);
+
+                if (board[i][j] == 'R') {
+                    rx = i;
+                    ry = j;
+                }
+
+                if (board[i][j] == 'B') {
+                    bx = i;
+                    by = j;
+                }
+            }
+        }
+
+        bfs(rx, ry, bx, by);
+
+        System.out.print(ans);
+    }
+
+    static void bfs(int rx, int ry, int bx, int by) {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[] {rx, ry, bx, by, 0, 4});
+
+        while (!q.isEmpty()) {
+            int[] poll = q.poll();
+
+            if (poll[4] == 10) { // 10번 이하까지의 노드들이 끝났으므로 불가
+                return;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                if (poll[5] == i) continue; // 이전에 같은 방향으로 기울인거는 다시 할 필요 x
+
+                Result result = move(i, poll);
+
+                if (board[result.bx][result.by] == 'O') continue; // 파랑이 빠진거면 불가
+
+                if (board[result.rx][result.ry] == 'O') {
+                    ans = poll[4] + 1;
+                    return;
+                }
+
+                q.offer(new int[] {result.rx, result.ry, result.bx, result.by, poll[4] + 1, i});
+            }
+        }
+    }
+
+    static Result move (int d, int[] arr) {
+        if (d == 0) {
+            return left(arr);
+        }
+
+        if (d == 1) {
+            return right(arr);
+        }
+
+        if (d == 2) {
+            return up(arr);
+        }
+
+        return down(arr);
+    }
+
+    static Result left(int[] arr) {
+        int rx = arr[0];
+        int ry = arr[1];
+        int bx = arr[2];
+        int by = arr[3];
+
+        if (rx == bx) { // 빨간공과 파란공이 같은 행에 위치해 있다면
+            // 누가 더 왼쪽에 위치해 있는지 확인
+            if (ry < by) { // 빨간 공이 더 왼쪽에 위치해 있다면
+                // 빨간공 왼쪽으로 이동
+                while (true) {
+                    if (board[rx][ry - 1] == '#') break;
+
+                    ry--;
+
+                    if (board[rx][ry] == 'O') break;
+                }
+
+                // 파란공을 두번째로 이동 -> 이때 빨간공 위치도 고려
+                while (true) {
+                    if (board[bx][by - 1] == '#') break;
+                    if (board[bx][by - 1] != 'O' && by - 1 == ry) break;
+
+                    by--;
+
+                    if (board[bx][by] == 'O') break;
+                }
+            } else { // 파란 공이 더 왼쪽에 위치해 있다면
+
+                // 파란공을 먼저 왼쪽으로 이동
+                while (true) {
+                    if (board[bx][by - 1] == '#') break;
+
+                    by--;
+
+                    if (board[bx][by] == 'O') break;
+                }
+
+                // 빨간 공을 두번째로 이동
+                while (true) {
+                    if (board[rx][ry - 1] == '#') break;
+                    if (board[rx][ry - 1] != 'O' && ry - 1 == by) break;
+
+                    ry--;
+
+                    if (board[rx][ry] == 'O') break;
+                }
+            }
+
+        } else { // 빨간공과 파란공이 다른 행에 위치해 있다면
+            // 빨간공 왼쪽으로 이동
+            while (true) {
+                if (board[rx][ry - 1] == '#') break;
+
+                ry--;
+
+                if (board[rx][ry] == 'O') break;
+            }
+
+            while (true) {
+                if (board[bx][by - 1] == '#') break;
+
+                by--;
+
+                if (board[bx][by] == 'O') break;
+            }
+        }
+
+        return new Result(rx, ry, bx, by);
+    }
+
+    static Result right(int[] arr) {
+        int rx = arr[0];
+        int ry = arr[1];
+        int bx = arr[2];
+        int by = arr[3];
+
+        if (rx == bx) { // 빨간 공과 파란 공이 같은 행에 위치해 있다면
+            if (ry < by) { // 파란 공이 더 오른쪽에 위치해 있다면
+                // 파란공을 먼저 오른쪽으로 이동시킨다
+                while (true) {
+                    if (board[bx][by + 1] == '#') break;
+
+                    by++;
+
+                    if (board[bx][by] == 'O') break;
+                }
+
+                // 빨간공을 두번째로 이동
+                while (true) {
+                    if (board[rx][ry + 1] == '#') break;
+                    if (board[rx][ry + 1] != 'O' && ry + 1 == by) break;
+
+                    ry++;
+
+                    if (board[rx][ry] == 'O') break;
+                }
+            } else { // 빨간 공이 더 오른쪽에 위치해 있다면
+                // 빨간을 먼저 오른쪽으로 이동시킨다
+                while (true) {
+                    if (board[rx][ry + 1] == '#') break;
+
+                    ry++;
+
+                    if (board[rx][ry] == 'O') break;
+                }
+
+                // 파란공을 두번째로 이동
+                while (true) {
+                    if (board[bx][by + 1] == '#') break;
+                    if (board[bx][by + 1] != 'O' && by + 1 == ry) break;
+
+                    by++;
+
+                    if (board[bx][by] == 'O') break;
+                }
+            }
+        } else { // 빨간공과 파란공이 다른 행에 위치해 있다면
+            // 빨간공을 오른쪽으로 이동시킨다
+            while (true) {
+                if (board[rx][ry + 1] == '#') break;
+
+                ry++;
+
+                if (board[rx][ry] == 'O') break;
+            }
+
+            // 파란공도 이동시킨다
+            while (true) {
+                if (board[bx][by + 1] == '#') break;
+
+                by++;
+
+                if (board[bx][by] == 'O') break;
+            }
+        }
+
+        return new Result(rx, ry, bx, by);
+    }
+
+    static Result up(int[] arr) {
+        int rx = arr[0];
+        int ry = arr[1];
+        int bx = arr[2];
+        int by = arr[3];
+
+        // 빨간공과 파란공이 같은 열에 위치해 있다면
+        if (ry == by) {
+            // 빨간 공이 더 위에 위치해 있다면
+            if (rx < bx) {
+                // 빨간공을 먼저 위로 이동 시킨다
+                while (true) {
+                    if (board[rx - 1][ry] == '#') break;
+
+                    rx--;
+
+                    if (board[rx][ry] == 'O') break;
+                }
+
+                // 파란공을 두번째로 위로 이동시킨다
+                while (true) {
+                    if (board[bx - 1][by] == '#') break;
+                    if (board[bx - 1][by] != 'O' && bx - 1 == rx) break;
+
+                    bx--;
+
+                    if (board[bx][by] == 'O') break;
+                }
+            } else { // 파란 공이 더 위에 위치해 있다면
+                // 파란을 먼저 위로 이동 시킨다
+                while (true) {
+                    if (board[bx - 1][by] == '#') break;
+
+                    bx--;
+
+                    if (board[bx][by] == 'O') break;
+                }
+
+                // 빨간공을 두번째로 위로 이동시킨다
+                while (true) {
+                    if (board[rx - 1][ry] == '#') break;
+                    if (board[rx - 1][ry] != 'O' && rx - 1 == bx) break;
+
+                    rx--;
+
+                    if (board[rx][ry] == 'O') break;
+                }
+            }
+        } else {
+            while (true) {
+                if (board[rx - 1][ry] == '#') break;
+
+                rx--;
+
+                if (board[rx][ry] == 'O') break;
+            }
+
+            while (true) {
+                if (board[bx - 1][by] == '#') break;
+
+                bx--;
+
+                if (board[bx][by] == 'O') break;
+            }
+        }
+
+        return new Result(rx, ry, bx, by);
+    }
+
+    static Result down(int[] arr) {
+        int rx = arr[0];
+        int ry = arr[1];
+        int bx = arr[2];
+        int by = arr[3];
+
+        // 빨간공과 파란공이 같은 열에 위치해 있다면
+        if (ry == by) {
+            // 빨간공이 더 아래에 위치해 있다면
+            if (rx > bx) {
+                // 빨간공을 먼저 아래로 이동시킨다
+                while (true) {
+                    if (board[rx + 1][ry] == '#') break;
+
+                    rx++;
+
+                    if (board[rx][ry] == 'O') break;
+                }
+
+                // 파란공을 두번째로 아래로 이동시킨다
+                while (true) {
+                    if (board[bx + 1][by] == '#') break;
+                    if (board[bx + 1][by] != 'O' && bx + 1 == rx) break;
+
+                    bx++;
+
+                    if (board[bx][by] == 'O') break;
+                }
+            } else { // 파란공이 더 아래에 위치해 있다면
+                // 파란공을 먼저 아래로 이동시킨다
+                while (true) {
+                    if (board[bx + 1][by] == '#') break;
+
+                    bx++;
+
+                    if (board[bx][by] == 'O') break;
+                }
+
+                // 빨간공을 두번째로 아래로 이동시킨다
+                while (true) {
+                    if (board[rx + 1][ry] == '#') break;
+                    if (board[rx + 1][ry] != 'O' && rx + 1 == bx) break;
+
+                    rx++;
+
+                    if (board[rx][ry] == 'O') break;
+                }
+            }
+        } else {
+            // 빨간공을 아래로 이동시킨다
+            while (true) {
+                if (board[rx + 1][ry] == '#') break;
+
+                rx++;
+
+                if (board[rx][ry] == 'O') break;
+            }
+
+            // 파란공을 아래로 이동시킨다
+            while (true) {
+                if (board[bx + 1][by] == '#') break;
+
+                bx++;
+
+                if (board[bx][by] == 'O') break;
+            }
+        }
+
+        return new Result(rx, ry, bx, by);
+    }
+}
