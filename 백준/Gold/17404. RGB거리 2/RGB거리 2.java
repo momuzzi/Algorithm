5e85b21a030ae0a990af2d6beb2b9f97b0b1b183
momuzzi <151748893+momuzzi@users.noreply.github.com>
2026-01-15 21:05:44 +0900

    [Gold IV] Title: RGB거리 2, Time: 116 ms, Memory: 15400 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.java
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -8,7 +8,6 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
 
         int[][] arr = new int[N][3];
-
         for (int i = 0; i < N; i++) {
             String[] rgb = br.readLine().split(" ");
             arr[i][0] = Integer.parseInt(rgb[0]);
@@ -16,103 +15,48 @@ public class Main {
             arr[i][2] = Integer.parseInt(rgb[2]);
         }
 
-        int[][] rdp = new int[N][3];
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(rdp[i], Integer.MAX_VALUE);
-        }
-        rdp[0][0] = arr[0][0];
-
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j < 3; j++) {
-
-                if (i < N - 1 && j == 0) {
-                    if (rdp[i - 1][1] != Integer.MAX_VALUE || rdp[i - 1][2] != Integer.MAX_VALUE) {
-                        rdp[i][j] = Math.min(rdp[i - 1][1], rdp[i - 1][2]) + arr[i][j];
-                    }
-                }
-
-                if (j == 1) {
-                    if (rdp[i - 1][0] != Integer.MAX_VALUE || rdp[i - 1][2] != Integer.MAX_VALUE) {
-                        rdp[i][j] = Math.min(rdp[i - 1][0], rdp[i - 1][2]) + arr[i][j];
-                    }
-                }
-
-                if (j == 2) {
-                    if (rdp[i - 1][0] != Integer.MAX_VALUE || rdp[i - 1][1] != Integer.MAX_VALUE) {
-                        rdp[i][j] = Math.min(rdp[i - 1][0], rdp[i - 1][1]) + arr[i][j];
-                    }
-                }
+        int[][][] dp = new int[3][N][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < N; j++) {
+                Arrays.fill(dp[i][j], Integer.MAX_VALUE);
             }
         }
-
-        int[][] gdp = new int[N][3];
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(gdp[i], Integer.MAX_VALUE);
-        }
-        gdp[0][1] = arr[0][1];
+        
+        dp[0][0][0] = arr[0][0];
+        dp[1][0][1] = arr[0][1];
+        dp[2][0][2] = arr[0][2];
 
         for (int i = 1; i < N; i++) {
             for (int j = 0; j < 3; j++) {
-                if (j == 0) {
-                    if (gdp[i - 1][1] != Integer.MAX_VALUE || gdp[i - 1][2] != Integer.MAX_VALUE) {
-                        gdp[i][j] = Math.min(gdp[i - 1][1], gdp[i - 1][2]) + arr[i][j];
-                    }
-                }
+                for (int k = 0; k < 3; k++) {
+                    if (i < N - 1 || k != j) {
+                        if (k == 0) {
+                            if (dp[j][i - 1][1] != Integer.MAX_VALUE || dp[j][i - 1][2] != Integer.MAX_VALUE) {
+                                dp[j][i][k] = Math.min(dp[j][i - 1][1], dp[j][i - 1][2]) + arr[i][0];
+                            }
+                        }
 
-                if (i < N - 1 && j == 1) {
-                    if (gdp[i - 1][0] != Integer.MAX_VALUE || gdp[i - 1][2] != Integer.MAX_VALUE) {
-                        gdp[i][j] = Math.min(gdp[i - 1][0], gdp[i - 1][2]) + arr[i][j];
-                    }
-                }
+                        if (k == 1) {
+                            if (dp[j][i - 1][0] != Integer.MAX_VALUE || dp[j][i - 1][2] != Integer.MAX_VALUE) {
+                                dp[j][i][k] = Math.min(dp[j][i - 1][0], dp[j][i - 1][2]) + arr[i][1];
+                            }
+                        }
 
-                if (j == 2) {
-                    if (gdp[i - 1][0] != Integer.MAX_VALUE || gdp[i - 1][1] != Integer.MAX_VALUE) {
-                        gdp[i][j] = Math.min(gdp[i - 1][0], gdp[i - 1][1]) + arr[i][j];
-                    }
-                }
-            }
-        }
-
-        int[][] bdp = new int[N][3];
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(bdp[i], Integer.MAX_VALUE);
-        }
-        bdp[0][2] = arr[0][2];
-
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (j == 0) {
-                    if (bdp[i - 1][1] != Integer.MAX_VALUE || bdp[i - 1][2] != Integer.MAX_VALUE) {
-                        bdp[i][j] = Math.min(bdp[i - 1][1], bdp[i - 1][2]) + arr[i][j];
-                    }
-                }
-
-                if (j == 1) {
-                    if (bdp[i - 1][0] != Integer.MAX_VALUE || bdp[i - 1][2] != Integer.MAX_VALUE) {
-                        bdp[i][j] = Math.min(bdp[i - 1][0], bdp[i - 1][2]) + arr[i][j];
-                    }
-                }
-
-                if (i < N - 1 && j == 2) {
-                    if (bdp[i - 1][0] != Integer.MAX_VALUE || bdp[i - 1][1] != Integer.MAX_VALUE) {
-                        bdp[i][j] = Math.min(bdp[i - 1][0], bdp[i - 1][1]) + arr[i][j];
+                        if (k == 2) {
+                            if (dp[j][i - 1][0] != Integer.MAX_VALUE || dp[j][i - 1][1] != Integer.MAX_VALUE) {
+                                dp[j][i][k] = Math.min(dp[j][i - 1][0], dp[j][i - 1][1]) + arr[i][2];
+                            }
+                        }
                     }
                 }
             }
         }
 
         int min = Integer.MAX_VALUE;
-
         for (int i = 0; i < 3; i++) {
-            min = Math.min(min, rdp[N - 1][i]);
-        }
-
-        for (int i = 0; i < 3; i++) {
-            min = Math.min(min, gdp[N - 1][i]);
-        }
-
-        for (int i = 0; i < 3; i++) {
-            min = Math.min(min, bdp[N - 1][i]);
+            for (int j = 0; j < 3; j++) {
+                min = Math.min(min, dp[i][N - 1][j]);
+            }
         }
 
         System.out.print(min);
